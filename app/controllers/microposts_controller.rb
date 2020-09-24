2010dcc0ee2575a66516a3d5db84733474586f72
mei <jgtop1010@gmail.com>
2020-09-24 16:20:10 +0000

    favorite, unfavorite

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -15,7 +15,7 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
-     @micropost.destroy
+    @micropost.destroy
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
   end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 20 }
+  
+  has_many :favorites, dependent: :destroy
+
 end
